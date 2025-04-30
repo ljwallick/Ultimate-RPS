adc294f0264e75a270b9b892c90f3c4cc3f77eb5
Landon <landonjwallick@gmail.com>
2025-04-30 08:28:18 -0500

    Update RPS_9.py
    Edited get_choice() to go through all items correctly to display them.

--- RPS_9.py
+++ RPS_9.py
@@ -80,11 +80,7 @@ def game_loop():
 
 def get_choice():
 	print('\n\nChoices:')
-	for choice in choices:
-		if choice[0] != 'gun':
-			print(f'{choice[0]}, ', end='')
-		else:
-			print('gun')
+	print(', '.join(choices[i][0] for i in range(9)))
 
 	computer_choice = random.randint(0, 8)
 	my_choice = input('\nWhat do you choose?\n\n    ')
